1f836a935abfe913b2275785140ecd0e6734217e
Yong-Jun Jo <77144571+yjjoooo@users.noreply.github.com>
2024-05-13 00:04:25 +0900

    Delete 프로그래머스/lv2 directory

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++
@@ -1,4 +0,0 @@
-def solution(s):
-    s = [int(x) for x in s.split(' ')]
-    answer = '{} {}'.format(min(s), max(s))
-    return answer
--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++
@@ -1,3 +0,0 @@
-def solution(s):
-    answer = ' '.join(list(map(lambda x : x.capitalize(), s.split(' '))))
-    return answer
